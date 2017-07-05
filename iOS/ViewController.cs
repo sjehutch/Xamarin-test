37ce3b72160f28c40919e8c6aee69c6d97807539
scott hutchinson <scotthutchinson@scotts-Mac-mini.local>
2017-07-04 20:48:57 -0500

    initial

---
+++ iOS/ViewController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using UIKit;
+
+namespace test.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+            Button.AccessibilityIdentifier = "myButton";
+            Button.TouchUpInside += delegate
+            {
+                if (amounttextfield.Text.Length < 1)
+                {
+                    return;
+                }
+                else {
+                    var result = Convert.ToDouble(amounttextfield.Text) * 0.69;
+                    resultlabel.Text = "$ "+ amounttextfield.Text + " in us dollars is equal to " + result.ToString() + "British pounds" ;
+                }
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.		
+        }
+    }
+}
---
+++ iOS/ViewController.designer.cs
@@ -0,0 +1,45 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace test.iOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        UIKit.UIButton Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField amounttextfield { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel resultlabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (amounttextfield != null) {
+                amounttextfield.Dispose ();
+                amounttextfield = null;
+            }
+
+            if (Button != null) {
+                Button.Dispose ();
+                Button = null;
+            }
+
+            if (resultlabel != null) {
+                resultlabel.Dispose ();
+                resultlabel = null;
+            }
+        }
+    }
+}
